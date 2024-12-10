1a8d01ba30a56155ba3b161275183dc732aa2042
Shist <shist.pupust@mail.ru>
2024-12-10 10:54:56 +0100

    Added component with list of hardcoded resources

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul>
+    <li v-for="resource in storesResources" :key="resource.id">
+      <ResourceCard
+        :title="resource.title"
+        :description="resource.description"
+        :link="resource.link"
+      />
+    </li>
+  </ul>
+</template>
+
+<script>
+import ResourceCard from '@/components/learning-resources/ResourceCard.vue';
+
+export default {
+  components: {
+    ResourceCard,
+  },
+  data() {
+    return {
+      storesResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/learning-resources/ResourceCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header>
+      <h2>{{ title }}</h2>
+      <button>Delete</button>
+    </header>
+    <p>{{ description }}</p>
+    <nav>
+      <a :href="link" target="_blank">View Resource</a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
